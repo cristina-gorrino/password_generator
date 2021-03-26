aec0818c88b40245161325d19eac7a38b34c4116
cgorrino <cgorrino03@gmail.com>
2021-03-25 22:49:44 -0700

    Adding arrays with password components.

--- script.js
+++ script.js
@@ -21,14 +21,18 @@ function generatePassword() {
 
   while (hasValidLength === false){
     if (passwordLength >= 8 && passwordLength <= 128) {
-      console.log("next question");
+      console.log("next question"); // REMOVE
       hasValidLength = true;
     } else {
       passwordLength = prompt("The password length must be least 8 characters and no more than 128. Please enter a number");
     }
   }
   
-  
+  var specialChar = [" ", "!", "\"", "#", "$", "%", "&", "'", "(", ")", "*", "+", ",", "-", ".", "/", ":", ";", "<", "=", ">", "?", "@", "[", "\\", "]", "^", "_", "`", "{", "|", "}", "~"];
+  var nums = [0,1,2,3,4,5,6,7,8,9];
+  var letters = "abcdefghijklmnopqrstuvwxyz";
+
+  console.log(specialChar);
 }
  
 
